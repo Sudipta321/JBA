e679865cc9417d19d3b0b74adc7a4dfdd94d7dc1
Sudipta321 <sarkar.sudipta123@gmail.com>
2015-01-18 01:03:47 +0530

    Spring MVC Test,DAY-1-18-01-2015

---
+++ src/main/java/org/java/spring/controller/IndexController.java
@@ -0,0 +1,16 @@
+package org.java.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping(value="/index")
+	public String index(Model model){
+		model.addAttribute("message","Sudipta");
+		return "hello";
+	}
+	
+
+}
